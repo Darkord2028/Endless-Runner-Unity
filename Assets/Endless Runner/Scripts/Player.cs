9f3aef2036b91fada47e099ddba4b37a06e9d842
Darkord2028 <98699507+Darkord2028@users.noreply.github.com>
2026-01-20 12:13:24 +0530

    Add: Lane Change with Lerp

--- Assets/Endless Runner/Scripts/Player.cs
+++ Assets/Endless Runner/Scripts/Player.cs
@@ -1,16 +1,114 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float laneChangeDuration = 0.3f;
+
+    [SerializeField] private Transform LeftTransform;
+    [SerializeField] private Transform MiddleTransform;
+    [SerializeField] private Transform RightTransform;
+
+    private int CurrentLaneIndex = 1; // 0 = Left, 1 = Middle, 2 = Right
+    private bool isChangingLane = false;
+
     void Start()
     {
-        
+        transform.position = MiddleTransform.position;
+        CurrentLaneIndex = 1;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+    private void MoveRight()
+    {
+        if (CurrentLaneIndex == 2 || isChangingLane)
+        {
+            Debug.Log("Already in Right Lane");
+            return;
+        }
+        else if (CurrentLaneIndex == 1)
+        {
+            CurrentLaneIndex++;
+        }
+        else if (CurrentLaneIndex == 0)
+        {
+            CurrentLaneIndex = 1;
+        }
+
+        StartCoroutine(ChangeLane(GetTargetPosition()));
+    }
+
+    private void MoveLeft()
+    {
+        if (CurrentLaneIndex == 0 || isChangingLane)
+        {
+            Debug.Log("Already in Left Lane");
+            return;
+        }
+        else if (CurrentLaneIndex == 1)
+        {
+            CurrentLaneIndex = 0;
+        }
+        else if (CurrentLaneIndex == 2)
+        {
+            CurrentLaneIndex = 1;
+        }
+
+        StartCoroutine(ChangeLane(GetTargetPosition()));
+    }
+
+    private IEnumerator ChangeLane(Vector3 TargetPosition)
+    {
+        isChangingLane = true;
+
+        Vector3 startPosition = transform.position;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < laneChangeDuration)
+        {
+            transform.position = Vector3.Lerp(startPosition, TargetPosition, (elapsedTime / laneChangeDuration));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = TargetPosition;
+        isChangingLane = false;
+    }
+
+    private Vector3 GetTargetPosition()
+    {
+        return CurrentLaneIndex switch
+        {
+            0 => LeftTransform.position,
+            1 => MiddleTransform.position,
+            2 => RightTransform.position,
+            _ => transform.position,
+        };
+    }
+
+    #region Input Functions
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Vector2 inputValue = context.ReadValue<Vector2>();
+            if (inputValue.x < 0)
+            {
+                MoveLeft();
+            }
+            else if (inputValue.x > 0)
+            {
+                MoveRight();
+            }
+        }
+    }
+
+    #endregion
+
 }
